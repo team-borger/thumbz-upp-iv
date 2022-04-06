ee9219cec289a616c0682821e6c6e764f9e6abab
ben <alanbenedictgolpeo@gmail.com>
2022-04-07 00:10:05 +0800

    try hard for webrtc

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from "react";
 import { Button, DrawerLayoutAndroid, Text, StyleSheet, View } from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const App = ({ navigation }) => {
   const drawer = useRef(null);
@@ -11,6 +12,18 @@ const App = ({ navigation }) => {
       setDrawerPosition("left");
     }
   };
+
+  const setValue = async () => {
+    try {
+      const value = await AsyncStorage.getItem('userId')
+      if(value !== null) {
+        console.log(value)
+        navigation.navigate('Call')
+      }
+    } catch(e) {
+      console.error(e)
+    }
+  }
 
   const navigationView = () => (
     <View style={[styles.container, styles.navigationContainer]}>
@@ -36,7 +49,8 @@ const App = ({ navigation }) => {
         />
         <Button
           title="Go to call"
-          onPress={() => navigation.navigate('Call')}
+          onPress={() => setValue()}
+          // onPress={() => navigation.navigate('Call')}
         />
       </View>
     </DrawerLayoutAndroid>
