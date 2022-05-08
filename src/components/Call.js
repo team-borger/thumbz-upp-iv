ab685b88d583cfad7aa20866a274e286c5ea0e75
ben <alanbenedictgolpeo@gmail.com>
2022-05-08 18:15:53 +0800

    cleaning ywow

--- src/components/Call.js
+++ src/components/Call.js
@@ -1,69 +1,13 @@
 import React from 'react';
 import { StyleSheet, View, Button, Text, TextInput, TouchableOpacity } from 'react-native';
-import { AuthService } from './../services';
+import { AuthService, CallService } from './../services';
 import ConnectyCube from 'react-native-connectycube';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import CallScreen from "../components/CallScreen";
 const Cat = ({ navigation }) => {
-  // let [lStream, setLocSteam] = React.useState(null);
 
-  let caleeId = ''
-  let localStream_ = ''
-  let session = ''
-  let extension = {}
-
-  const connectyCube = async () => {
-    let calleesIds = []; // User's ids
-
-    const userId = await AsyncStorage.getItem('userId')
-    if(userId == '5757268') calleesIds.push(5744964)
-    else calleesIds.push(5757268)
-
-    caleeId = Number(userId)
-
-    const sessionType = ConnectyCube.videochat.CallType.VIDEO; // AUDIO is also possible
-    const additionalOptions = { bandwidth: 256 };
-    session = ConnectyCube.videochat.createNewSession(calleesIds, sessionType, additionalOptions);
-
-    const mediaParams = {
-      audio: true,
-      video: true
-    };
-
-    session
-      .getUserMedia(mediaParams)
-      .then((localStream) => {
-        console.log('on sessionCreate', localStream)
-        localStream_ = localStream
-        // setLocSteam(localStream_)
-        initCall()
-      })
-      .catch((error) => {
-        console.error('session error', error)
-      });
-
-    // console.log('test', session)
-  };
-
-  const initCall = () => {
-    session.call(extension, (error) => {
-      console.log('xtension: ', extension)
-      console.log('err: ', error)
-
-    });
-    // AsyncStorage.setItem('caleeId', caleeId)
-    // AsyncStorage.setItem('localStream', JSON.stringify(localStream_))
-    <CallScreen
-      calee={caleeId}
-      locStream={localStream_.toURL()}
-    >
-    </CallScreen>
+  const startCall = async () => {
+    CallService.startCall()
     navigation.navigate('CallScreen')
-    // console.log("asdsa", localStream_.toURL())
-  };
-
-  const stopCall = () => {
-    session.stop(extension);
   };
 
   const logout = () => {
@@ -73,7 +17,7 @@ const Cat = ({ navigation }) => {
 
   return (
     <View>
-      <TouchableOpacity onPress={() => connectyCube()}>
+      <TouchableOpacity onPress={() => startCall()}>
         <View
           style={[styles.authBtn, styles.centeredChildren]}>
           <Text style={styles.authBtnText}>
@@ -81,7 +25,7 @@ const Cat = ({ navigation }) => {
           </Text>
         </View>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => stopCall()}>
+      <TouchableOpacity onPress={() => CallService.stopCall()}>
         <View
           style={[styles.authBtn, styles.centeredChildren]}>
           <Text style={styles.authBtnText}>
--- src/services/call-service.js
+++ src/services/call-service.js
@@ -1,8 +1,93 @@
 import { Platform, ToastAndroid } from 'react-native';
 import ConnectyCube from 'react-native-connectycube';
+import CallScreen from "../components/CallScreen";
 import InCallManager from 'react-native-incall-manager';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Sound from 'react-native-sound';
 
 export default class CallService {
+  static MEDIA_OPTIONS = { audio: true, video: { facingMode: 'user' } };
+  _session = null;
+  mediaDevices = [];
 
+  outgoingCall = new Sound(require('../assets/audio/dialing.mp3'));
+  incomingCall = new Sound(require('../assets/audio/calling.mp3'));
+  endCall = new Sound(require('../assets/audio/end_call.mp3'));
+
+  setMediaDevices() {
+    return ConnectyCube.videochat.getMediaDevices().then(mediaDevices => {
+      this.mediaDevices = mediaDevices;
+    });
+  }
+
+  startCall = async () => {
+    let calleesIds = []; // User's ids
+
+    const userId = await AsyncStorage.getItem('userId')
+    if(userId == '5757268') calleesIds.push(5744964)
+    else calleesIds.push(5757268)
+
+    caleeId = Number(userId)
+
+    const sessionType = ConnectyCube.videochat.CallType.VIDEO; // AUDIO is also possible
+    const additionalOptions = { bandwidth: 256 };
+    this._session = ConnectyCube.videochat.createNewSession(calleesIds, sessionType, additionalOptions);
+
+    return this._session
+      .getUserMedia(CallService.MEDIA_OPTIONS)
+      .then((stream) => {
+        console.log('on sessionCreate', stream)
+        this._session.call({});
+
+        <CallScreen
+          calee={caleeId}
+          locStream={stream.toURL()}
+        >
+        </CallScreen>
+        return stream
+      })
+      .catch((error) => {
+        console.error('session error', error)
+      });
+  };
+
+  stopCall = () => {
+    this.stopSounds();
+
+    if (this._session) {
+      this.playSound('end')
+      this._session.stop({});
+      ConnectyCube.videochat.clearSession(this._session.ID);
+      this._session = null;
+      this.mediaDevices = [];
+    }
+  };
+
+  playSound = type => {
+    switch (type) {
+      case 'outgoing':
+        this.outgoingCall.setNumberOfLoops(-1);
+        this.outgoingCall.play();
+        break;
+      case 'incoming':
+        this.incomingCall.setNumberOfLoops(-1);
+        this.incomingCall.play();
+        break;
+      case 'end':
+        this.endCall.play();
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  stopSounds = () => {
+    if (this.incomingCall.isPlaying()) {
+      this.incomingCall.pause();
+    }
+    if (this.outgoingCall.isPlaying()) {
+      this.outgoingCall.pause();
+    }
+  };
 }
