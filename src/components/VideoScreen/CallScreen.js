d501ca81fc92993bfbe9193c573c36ddd36866a6
rhinlog <raymunddh@gmail.com>
2022-05-16 23:02:07 +0800

    sa master nko gi push

--- src/components/VideoScreen/CallScreen.js
+++ src/components/VideoScreen/CallScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Button, Text, TextInput } from 'react-native';
+import { StyleSheet, View, Button, Text, TextInput,Dimensions } from 'react-native';
 import ConnectyCube from 'react-native-connectycube';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { RTCView } from 'react-native-connectycube';
@@ -11,15 +11,15 @@ const CallScreen = (response) => {
     if(res.localKey) {
       return (
         <View style={styles.blackView}>
-          <RTCView style={styles.rtcView} objectFit="cover" key={res.localKey} streamURL={res.localStream.toURL()} />
-          <RTCView style={styles.rtcView} objectFit="cover" key={res.remoteKey} streamURL={res.remoteStream.toURL()} />
+          <RTCView style={styles.localKey} key={res.localKey} streamURL={res.localStream.toURL()} />
+          <RTCView style={styles.remoteKey} objectFit="cover" key={res.remoteKey} streamURL={res.remoteStream.toURL()} />
         </View>
       );
     }
     else {
       return (
         <View style={styles.blackView}>
-          <RTCView style={styles.rtcView} objectFit="cover" key={res.response.calee} streamURL={res.response.stream.toURL()} />
+          <RTCView style={styles.localKey} objectFit="cover" key={res.response.calee} streamURL={res.response.stream.toURL()} />
         </View>
       );
     }
@@ -35,9 +35,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'black',
   },
-  rtcView: {
-    height: 150,
-    width: 100
+  localKey: {
+    height: 180,
+    width: 150,
+    position:'absolute',
+    top:10,
+    left:-20,
+  },
+  remoteKey: {
+    position:'relative',
+    width: Dimensions.get('window').width,
+	  height: Dimensions.get('window').height
   },
 });
 
